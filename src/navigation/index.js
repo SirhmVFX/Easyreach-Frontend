a83d297adefd8718b1cb58070ccf3e8d56b1df69
Solomon Olatunji <realsolomon01@outlook.com>
2023-06-17 20:41:25 +0100

    register

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Homepage from "../pages/Home/Homepage";
+import Register from "../pages/Auth/Register";
 
 function Navigation() {
     return (
@@ -8,6 +9,7 @@ function Navigation() {
                 <BrowserRouter>
                 <Routes>
                     <Route path="/" element={<Homepage />} />
+                    <Route path="/register" element={<Register />} />
                 </Routes>
                 </BrowserRouter>
             </main>
