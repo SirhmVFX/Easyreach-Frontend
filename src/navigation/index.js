3678ae7441e7f7d7468a6f12a1281c1300bfef7f
Solomon Olatunji <realsolomon01@outlook.com>
2023-06-18 08:29:15 +0100

    login added

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Homepage from "../pages/Home/Homepage";
 import Register from "../pages/Auth/Register";
+import Login from "../pages/Auth/Login";
 
 function Navigation() {
     return (
@@ -10,6 +11,7 @@ function Navigation() {
                 <Routes>
                     <Route path="/" element={<Homepage />} />
                     <Route path="/register" element={<Register />} />
+                    <Route path="/login" element={<Login />} />
                 </Routes>
                 </BrowserRouter>
             </main>
